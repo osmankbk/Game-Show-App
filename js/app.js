918ec0a8749a1e72257d33c1841b4937a2b46390
osmankbk <mackiekbk@yahoo.com>
2021-02-26 08:22:29 -0500

    Gets elements from the html file as saves them to variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 
+//Set onscreen keyboard keys container to onScreenKeys.
+const onScreenKeys = document.querySelectorAll('#qwerty');
+//Set guessing pharase to phraseToGuess.
+const phraseToGuess = document.querySelector('#phrase');
+//Set lives lost to missed.
+let missed = 0;
