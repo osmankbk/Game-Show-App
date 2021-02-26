8480ae3da8a17748651acba7fd41b5a38bfb56af
osmankbk <mackiekbk@yahoo.com>
2021-02-26 17:32:56 -0500

    Add a semicolon to phrases array

--- js/phrase.js
+++ js/phrase.js
@@ -1,7 +1,8 @@
+//Set list of phrases to phrases variable.
 let phrases = [
     'Perforce there is one there must be one',
     'Mometo Mori',
     'Black King',
     'Wonder is food for the soul',
-    'I love Because I love'
-]
+    'I love Because I love',
+];
