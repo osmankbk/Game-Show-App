8b67e4f7283e3420f3cf986f049e563bff32587d
osmankbk <mackiekbk@yahoo.com>
2021-02-26 08:28:31 -0500

    Creates phrase.js file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-
+//Set start game button to start variable
+const start = document.querySelector('.start');
 //Set onscreen keyboard keys container to onScreenKeys.
 const onScreenKeys = document.querySelectorAll('#qwerty');
 //Set guessing pharase to phraseToGuess.
 const phraseToGuess = document.querySelector('#phrase');
 //Set lives lost to missed.
 let missed = 0;
+
