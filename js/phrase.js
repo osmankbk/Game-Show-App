d2fdd93e1d82ddc80114627c976fff9c47f14058
osmankbk <mackiekbk@yahoo.com>
2021-03-09 16:47:20 -0500

    Adds more quotes(very funny one) to phrase.js

--- js/phrase.js
+++ js/phrase.js
@@ -1,8 +1,18 @@
 //Set list of phrases to phrases variable.
 let phrases = [
-    'Perforce there is one there must be one',
-    'Mometo Mori',
     'Black King',
-    'Wonder is food for the soul',
     'I love Because I love',
+    "My six pack is protected by a layer of fat",
+    'Wonder is food for the soul',
+    "I love my job only when I am on vacation",
+    "The road to success is always under construction",
+    "This sentence is a lie",
+    'Perforce there is one there must be one',
+    "I get enough exercise pushing my luck",
+    "Change is good but dollars are better",
+    "I am not lazy I am just very relaxed",
+    'Mometo Mori',
+    "A cookie a day keeps the sadness away",
+    "A balanced diet means a cupcake in each hand",
+    "You live learn and upgrade"
 ];
